d3490a073efdfd6467bceb4ec20191fb15316d1b
Mike <mikec201722@gmail.com>
2018-04-22 09:47:39 -0700

    Fixed errors
    Able to find the longest subsequence of strings of different lengths

--- MainCodeArea.java
+++ MainCodeArea.java
@@ -33,63 +33,82 @@ public class MainCodeArea {
       String s1 = ar.get(0);
       String s2 = ar.get(1);
       // String s3 = ar.get(2);
-      String[] s1ar = s1.split("", 0);
-      String[] s2ar = s2.split("", 0);
+      String[] rowS1 = s1.split("", 0);
+      String[] colS2 = s2.split("", 0);
       // String[] s3ar = s3.split("", 0);
-      int[][] twoD = new int[s1ar.length+1][s2ar.length+1]; 
-      for (int i = 0; i <= s1ar.length ; i++ ) { 
-         for (int j = 0; j <= s2ar.length ; j++ ) {
+      int[][] twoD = new int[rowS1.length+1][colS2.length+1]; 
+      for (int i = 0; i <= rowS1.length ; i++ ) { 
+         for (int j = 0; j <= colS2.length ; j++ ) {
             twoD[i][j] = 0;
          }
       }
-      printArrays(twoD,s1ar,s2ar);
+      //printArrays(twoD,rowS1,colS2);
       int addThis = 0;
       int rowDec =0;
       int colDec =0;
       int max = 0;
       String sOne ="";
       String sTwo ="";
-      for (int row = 1; row <= s1ar.length ; row++ ) { 
-         for (int col = 1; col <= s2ar.length ; col++ ) {
-            sTwo=s2ar[row-1];
-            sOne=s1ar[col-1];
-            
-            if(sTwo.equals(sOne)){
-               addThis = twoD[row-1][col-1];
-               twoD[row][col] = addThis+1;
-            }
-            else{
-               rowDec = twoD[row-1][col];
-               colDec = twoD[row][col-1];
-               max = Math.max(rowDec,colDec);
-               twoD[row][col] = max;
-            }
-         }
+      String output ="";
+      output += "\nrowS1.length: "+rowS1.length+" colS2.length: "+colS2.length+" \n\n";
+      for (int row = 1; row <= rowS1.length ; row++ ) { 
+		 for (int col = 1; col <= colS2.length ; col++ ) {
+		 	// if((col-1) < colS2.length)
+		 	// 	sTwo=colS2[col-1];
+		 	// else
+		 	// 	break;
+		 	// if((row-1) < rowS1.length)
+		 	// 	sOne=rowS1[row-1];
+		 	// else
+		 	// 	break;
+		 	
+		 	sTwo=colS2[col-1];
+		 	sOne=rowS1[row-1];
+		 	// sTwo=colS2[row-1];
+		 	// sOne=rowS1[col-1];
+		    if(sTwo.equals(sOne)){
+		    	output += "\n\n\nsTwo: "+ sTwo+ " sOne: "+ sOne+ " \nrow: "+row+" col: "+col+" \nrow-1: "+(row-1)+" col-1: "+(col-1)+"\n\n\n\n";
+		        addThis = twoD[row-1][col-1];
+		        twoD[row][col] = addThis+1;
+		    }
+		    else{
+		    	output += "\nsTwo: "+ sTwo+ " sOne: "+ sOne+ " \nrow: "+row+" col: "+col+" \nrow-1: "+(row-1)+" col-1: "+(col-1)+"\n\n";
+		       rowDec = twoD[row-1][col];
+		       colDec = twoD[row][col-1];
+		       max = Math.max(rowDec,colDec);
+		       twoD[row][col] = max;
+		    }
+		 }
       }
-      printArrays(twoD,s1ar,s2ar);
+      
+      int sub = twoD[rowS1.length][colS2.length]; 
+      printArrays(twoD,rowS1,colS2);
+      //System.out.println(output);
+
+      System.out.println("Longest sequence is "+ sub);
 
 
    }
 
-   public void printArrays(int[][] ar,String[]s1,String[]s2){
+   public void printArrays(int[][] ar,String[]rowS1,String[]colS2){
       String output ="          ";
       
-      for (int i = 0; i < s1.length ; i++ ) { 
-         output += "| "+s1[i] +" |";
+      for (int i = 0; i < colS2.length ; i++ ) { 
+         output += "| "+colS2[i] +" |";
 
       }
 
       output += "\n";
 
-      for (int i = 0; i < ar.length ; i++ ) { 
-         if(i == 0)
-         output += "|   |";
-         else if(i <= s2.length)
-         output += "| "+s2[i-1] +" |";
+      for (int row = 0; row <= rowS1.length ; row++ ) { 
+         if(row == 0)
+         output += "     ";
+         else if(row <= rowS1.length)
+         output += "| "+rowS1[row-1] +" |";
          else
          output += "     ";
-         for (int j = 0; j < ar[0].length ; j++ ) {
-            output += "| "+ar[i][j] +" |";
+         for (int col = 0; col <= colS2.length ; col++ ) {
+            output += "| "+ar[row][col] +" |";
          }
          output+="\n";
       }
--- Project2.java
+++ Project2.java
@@ -3,8 +3,8 @@ import java.util.ArrayList;
 
 public class Project2 {
 
-	public static void main(String[] args) {
-		MainCodeArea obj = new MainCodeArea();
-		obj.readFile(); 
-	}
+   public static void main(String[] args) {
+      MainCodeArea obj = new MainCodeArea();
+      obj.readFile(); 
+   }
 }
